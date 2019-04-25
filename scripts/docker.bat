a79443857d70b714bb2e582d9188197814c86550
Ezekiel Warren <zekewarren@gmail.com>
2019-04-25 00:57:34 -0700

    Updated gcc in docker script

--- scripts/docker.bat
+++ scripts/docker.bat
@@ -4,4 +4,4 @@ docker run -it ^
   --entrypoint /bin/bash ^
   --mount type=bind,source="%cd%",target=/root/projects/libecs ^
   --workdir="/root/projects/libecs" ^
-  zaucy/bazel:0.15.0-gcc-8
+  zaucy/bazel:gcc-8
